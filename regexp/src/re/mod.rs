50fb68fe21d34a1913839ec86df85ad29af5efe3
Getty Ritter <gdritter@galois.com>
2014-08-28 19:12:14 -0700

    Fixed import problem

--- regexp/src/re/mod.rs
+++ regexp/src/re/mod.rs
@@ -1,7 +1,5 @@
 pub use re::compile::compile;
 pub use re::instruction::{Instr,IChar,IMatch,IJmp,ISplit};
-pub use re::recursive::eval;
-pub use re::stack::eval;
 pub mod compile;
 pub mod instruction;
 pub mod recursive;
