82429f04e08ee6321b9f9807be0bf6349a9e1805
Getty Ritter <gdritter@galois.com>
2015-05-22 11:39:57 -0700

    Updated lcalc to Rust stable

--- lcalc/src/main.rs
+++ lcalc/src/main.rs
@@ -1,11 +1,13 @@
+use std::thread;
+
 // This isn't a very Rust-ey lambda-calculus implementation. It's much
 // more TAPL-ey, which I think makes it nice for pedagogical purposes
 // when aimed at functional programmers, but you wouldn't actually
 // use Rust like this in practice.
 
-#[deriving(Eq,PartialEq,Clone,Show)]
+#[derive(Eq,PartialEq,Clone,Debug)]
 enum Term {
-    Num(int),
+    Num(i32),
     Var(String),
     Lam(String, Box<Term>),
     App(Box<Term>, Box<Term>),
@@ -14,40 +16,48 @@ enum Term {
 
 // The following are wrappers over λ-terms to simplify writing
 // allocations. It really does help, as you can see in main.
-fn num(n: int) -> Box<Term> {
-    box Term::Num(n)
+fn num(n: i32) -> Box<Term> {
+    Box::new(Term::Num(n))
 }
 
 fn var(s: &str) -> Box<Term> {
-    box Term::Var(s.to_string())
+    Box::new(Term::Var(s.to_string()))
 }
 
 fn lam(x: &str, n: Box<Term>) -> Box<Term> {
-    box Term::Lam(x.to_string(), n)
+    Box::new(Term::Lam(x.to_string(), n))
 }
 
 fn app(x: Box<Term>, y: Box<Term>) -> Box<Term> {
-    box Term::App(x, y)
+    Box::new(Term::App(x, y))
 }
 
 fn let_(x: &str, y: Box<Term>, z: Box<Term>) -> Box<Term> {
-    box Term::Let(x.to_string(), y, z)
+    Box::new(Term::Let(x.to_string(), y, z))
 }
 
 // A value is either a number or a closure, which has to have
 // its environment around. We'll have to clone the environment
 // into the closure to make sure that it stays around even if
 // the closure is returned from the environment where it was used.
-#[deriving(Eq,PartialEq,Clone,Show)]
+#[derive(Eq,PartialEq,Clone,Debug)]
 enum Val {
-    Num(int),
+    Num(i32),
     Lam(String, Box<Term>, Box<Env>),
+}
+
+fn vnum(n: i32) -> Box<Val> {
+    Box::new(Val::Num(n))
+}
+
+fn vlam(x: String, b: Box<Term>, e: Box<Env>) -> Box<Val> {
+    Box::new(Val::Lam(x, b, e))
 }
 
 // I could also use a pair of a map and a parent pointer, but
 // this is a little more TAPL-ish. Plus, we generally always
 // bind a single variable at a time.
-#[deriving(Eq,PartialEq,Clone,Show)]
+#[derive(Eq,PartialEq,Clone,Debug)]
 enum Env {
     Empty,
     Binding(String, Box<Val>, Box<Env>),
@@ -73,31 +83,27 @@ fn lookup(s: &String, e: &Env) -> Box<Val> {
 // ownership.
 fn lcalc_eval(t: &Term, e: &Env) -> Box<Val> {
     match t {
-        &Term::Num(num) => {
-            box Val::Num(num)
-        }
-        &Term::Var(ref str) => {
-            lookup(str, e)
-        }
+        &Term::Num(num) => vnum(num),
+        &Term::Var(ref str) => lookup(str, e),
         &Term::Lam(ref s, ref b) => {
-            box Val::Lam(s.clone(), b.clone(), box e.clone())
+            vlam(s.clone(), b.clone(), Box::new(e.clone()))
         }
-        &Term::App(box ref f, box ref x) => {
+        &Term::App(ref f, ref x) => {
             match *lcalc_eval(f, e) {
-                Val::Lam(ref arg, box ref body, box ref env) => {
-                     let new_env = Env::Binding(arg.clone(),
-				               lcalc_eval(x, e),
-                                               box env.clone());
+                Val::Lam(ref arg, ref body, ref env) => {
+                    let new_env = Env::Binding(arg.clone(),
+                                               lcalc_eval(x, e),
+                                               Box::new(*env.clone()));
                      lcalc_eval(body, &new_env)
                   }
                 _ => panic!("Tried to apply a non-function!")
             }
         }
-        &Term::Let(ref s, box ref t, box ref b) => {
+        &Term::Let(ref s, ref t, ref b) => {
              let new_env =
-	     	 Env::Binding(s.clone(),
-			      lcalc_eval(t, e),
-                              box e.clone());
+             Env::Binding(s.clone(),
+                          lcalc_eval(t, e),
+                          Box::new(e.clone()));
              lcalc_eval(b, &new_env)
         }
     }
@@ -108,7 +114,7 @@ fn lcalc_eval(t: &Term, e: &Env) -> Box<Val> {
 fn lcalc_eval_opt(t: &Term, e: &Env) -> Option<Box<Val>> {
     let new_term = t.clone();
     let new_env = e.clone();
-    let guard = std::thread::Thread::spawn(move || {
+    let guard = thread::spawn(move || {
         lcalc_eval(&new_term, &new_env)
     });
     match guard.join() {
@@ -133,8 +139,8 @@ fn main() {
     // (2)(3), which will also obviously fail
     let s4 = app(num(2), num(3));
     let e = Env::Empty;
-    println!("s1: {:}", lcalc_eval_opt(&*s1, &e));
-    println!("s2: {:}", lcalc_eval_opt(&*s2, &e));
-    println!("s3: {:}", lcalc_eval_opt(&*s3, &e));
-    println!("s4: {:}", lcalc_eval_opt(&*s4, &e));
+    println!("s1: {:?}", lcalc_eval_opt(&*s1, &e));
+    println!("s2: {:?}", lcalc_eval_opt(&*s2, &e));
+    println!("s3: {:?}", lcalc_eval_opt(&*s3, &e));
+    println!("s4: {:?}", lcalc_eval_opt(&*s4, &e));
 }
